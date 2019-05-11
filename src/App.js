50b97ced0ae81dc9307e59ca62c25e6ed03e8acc
meeqmaut <victoriovincitore@gmail.com>
2019-05-11 15:08:28 +0300

    rewrite to requestanimationframe style

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
 import React from 'react';
 import './App.css';
-import moment from 'moment';
+import Clock from './Components/Clock';
+
 class App extends React.Component {
   render () {
     return (
       <div>
         <Clock/>
-      </div>
-    );
-  }
-}
-
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date:new Date()
-    };
-  }
-  
-
-  componentDidMount() {
-    this.timer = setInterval(() => this.tick(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timer);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-
-  }
-
-  render () {
-    return (
-      <div className="clock">
-        <h1>oClock</h1>
-        <h3>{ moment(this.state.date.toLocaleString()).format('MMMM DD, YYYY, h:mm:ss')}</h3>
       </div>
     );
   }
